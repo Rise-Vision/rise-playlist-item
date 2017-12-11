9e35c4a46466c0409d90f2315e1f82d39d0b1716
rodrigopavezi <rodrigopavezi@gmail.com>
2017-12-11 12:30:26 -0200

    First setup

---
+++ src/rise-playlist-item.js
@@ -0,0 +1,10 @@
+import { WebComponent } from 'web-component';
+
+@WebComponent('rise-playlist-item', {
+  template: require('./rise-playlist-item.html'),
+  shadowDOM: true
+})
+
+export default class RisePlaylistItem extends HTMLElement {
+
+}
